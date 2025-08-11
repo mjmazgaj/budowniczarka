c8d62ee884653ec48c0d838716d2629e044c4ad7
Mazgaj <>
2025-08-11 20:42:19 +0200

    Add Intro draft

--- src/Components/Home/Intro/Intro.jsx
+++ src/Components/Home/Intro/Intro.jsx
@@ -1,7 +1,20 @@
+import "./intro.css";
+
 const Intro = () => {
   return (
     <section id="intro">
-      <h3>Intro</h3>
+      <div className="intro-description_container">
+        <div className="intro-description_title_container">
+          <h3 className="intro-description_title">OGARNIEMY TO</h3>
+          <h3 className="intro-description_title">RAZEM!</h3>
+        </div>
+        <div className="intro-description_details_container">
+          <p>Świadectwa Charakterystyki Energetycznej</p>
+          <p>Egzamin na uprawnienia budowlane</p>
+          <p>Nadzór prac budowlanych</p>
+        </div>
+      </div>
+      <div className="intro-image_container"></div>
     </section>
   );
 };
