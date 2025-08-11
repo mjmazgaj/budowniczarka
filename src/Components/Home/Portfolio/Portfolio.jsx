5215e58f4a0c8709963ad7660efb3370f85b6a73
Mazgaj <>
2025-08-11 21:09:57 +0200

    Add Info portfolio

--- src/Components/Home/Portfolio/Portfolio.jsx
+++ src/Components/Home/Portfolio/Portfolio.jsx
@@ -1,7 +1,23 @@
+import "./portfolio.css";
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
-      <h3>Portfolio</h3>
+      <h2 className="portfolio-title_text">PRODUKTY</h2>
+      <div className="portfolio_container">
+        <div className="portfolio_button">
+          <p>SZCZEGÓŁOWE SPIS TREŚCI</p>
+          <p>PRAWO BUDOWLANE</p>
+        </div>
+        <div className="portfolio_button">
+          <p>ŚWIADECTWO CHARAKTERYSTYKI</p>
+          <p>ENERGETYCZNEJ</p>
+        </div>
+        <div className="portfolio_button">
+          <p>WYKOŃCZENIE POD KLUCZ - </p>
+          <p>NADZÓR PRAC</p>
+        </div>
+      </div>
     </section>
   );
 };
