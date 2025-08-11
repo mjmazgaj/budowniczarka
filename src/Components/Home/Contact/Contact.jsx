7c7f24964b8a92c07efec1da14aec1d2b9d13b2e
Mazgaj <>
2025-08-11 21:26:15 +0200

    Add contact draft

--- src/Components/Home/Contact/Contact.jsx
+++ src/Components/Home/Contact/Contact.jsx
@@ -1,7 +1,29 @@
+import "./contact.css";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { TiMessageTyping } from "react-icons/ti";
+
 const Contact = () => {
   return (
     <section id="contact">
-      <h3>Contact</h3>
+      <h2 className="contact-title_text">KONTAKT</h2>
+      <div className="contact-socialmedia_container">
+        <div className="contact-socialmedia_item">
+          <FaInstagram size={70} />
+          <h3>INSTAGRAM</h3>
+          <p>@budowniczarka</p>
+        </div>
+        <div className="contact-socialmedia_item">
+          <FaTiktok size={70} />
+          <h3>TIK TOK</h3>
+          <p>@budowniczarka_</p>
+        </div>
+        <div className="contact-socialmedia_item">
+          <TiMessageTyping size={70} />
+          <h3>E-MAIL</h3>
+          <p>budowniczarka@gmail.com</p>
+        </div>
+      </div>
     </section>
   );
 };
